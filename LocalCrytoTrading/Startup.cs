b20758b3ceb080c08eaa44c3c325291d365e2d7c
Franck Nana Ngassa <fngassa@rbbn.com>
2020-12-13 23:08:34 -0500

    Forward service

--- LocalCrytoTrading/Startup.cs
+++ LocalCrytoTrading/Startup.cs
@@ -56,15 +56,16 @@ namespace LocalCrytoTrading
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
 
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
 
             app.UseAuthentication();
             app.UseAuthorization();
